15ec4dafe17fa9b25a012b872139555441ac2fde
fengtao666 <15945534786@163.com>
2020-01-21 10:34:39 +0800

    ft底部当行样式完成

--- youxiake/src/components/mBottomNav/mBottomNav.vue
+++ youxiake/src/components/mBottomNav/mBottomNav.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="mBottomNav">
-        <a href="#/">
+  <div class="mBottomNav">
+    <router-link v-for="(item,index) in tab" :key="index" :to="item.path" active-class >
+      <i class="iconfont" @click="change(index)">{{item.pic}}</i>
+      {{item.name}}
+    </router-link>
+    <!-- <a href="#/">
         <i class="iconfont">&#xe9ce;</i>
         首页
         </a>
@@ -11,38 +15,63 @@
         <a href="#/userCenter">
         <i class="iconfont">&#xe677;</i>
         个人中心
-        </a>
-    </div>
+    </a>-->
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      tab: [
+        { name: "首页", path: "/", pic: "\ue9ce" },
+        { name: "订单", path: "/order", pic: "\ue643" },
+        { name: "个人中心", path: "/userCenter", pic: "\ue677" }
+      ]
+    };
+  },
+  methods: {
+    change(index) {
+      switch (index) {
+        case 0:
+          this.tab[index].pic = "\ue7eb";
+          break;
+        case 1:
+          this.tab[index].pic = "\ue860";
+          break;
+        case 2:
+          this.tab[index].pic = "\ue71f";
+          break;
+      }
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
 @import "~style/index.less";
-.mBottomNav{
-    .w(375);
-    .h(55);
-    position: fixed;
-    bottom: 0;
-    background-color: @bgColor;
-    display: flex;
-    flex-direction: row;
-    a{
-        color: #999;
-        font-size: 14px;
-        text-align: center;
-        .w(200);
-        .h(30);
-        .lh(30);
-        i{
-            display:block;
-            font-size: 24px;
-  
-        }
+.mBottomNav {
+  .w(375);
+  .h(55);
+  position: fixed;
+  bottom: 0;
+  background-color: @bgColor;
+  display: flex;
+  flex-direction: row;
+  a {
+    font-size: 14px;
+    text-align: center;
+    color: #666;
+    .w(200);
+    .h(30);
+    .lh(30);
+    i {
+      display: block;
+      font-size: 24px;
     }
+  }
+  .router-link-exact-active {
+    color: red;
+  }
 }
 </style>
